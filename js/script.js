ed91a2e6ff6b3e0f5b36dad320ddcbae3be334ee
lucas souza <lucas2469y@gmail.com>
2025-07-28 17:56:09 -0300

    aqui eu atualizei o css e js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function confirmar() {
+  
+  let nome = document.getElementById('nome').value.trim()
+  let numero = document.getElementById('numero').value
+  let mes = document.getElementById('mes').value
+  let ano = document.getElementById('ano').value
+  let cvc = document.getElementById('cvc').value
+let erro = document.getElementById('erro')
+
+  // aqui e pora deixar os campos validos 
+  let valido = true
+
+  if (nome === '' || numero.length < 16 || mes === '' || ano === '' || cvc.length < 3) {
+    window.alert('Por favor, preencha todos os campos corretamente!')
+    erro.style.display = 'block'
+    valido = false
+  } else {
+    erro.style.display = 'none'
+  }
+
+  if (!valido) return
+
+  // aqui vai ta estar formatando o numero do cartao para ter o espaçamentos a cada 4 numeros 
+  let numeroFormatado = numero.replace(/(.{4})/g, '$1 ')
+
+  document.getElementById('nome-cartao').innerHTML = nome
+  document.getElementById('numero-cartao').innerHTML = `<p>${numeroFormatado}</p>`
+  document.getElementById('vencimento-cartao').innerHTML = `${mes}/${ano}`
+  document.getElementById('cvc-cartao').innerHTML = cvc
+
+  document.querySelector('.formulario-cartao').style.display = 'none'
+  document.getElementById('mensagem-sucesso').style.display = 'flex'
+}
+
+function recarregar() {
+  location.reload()
+}
